453fc272f099e9fafba77f1526c468e95c6add9a
Pratham Attri <attripratham2062@gmail.com>
2024-08-30 17:30:45 +0530

    Added file cleanup after upload in upoad utility  - Minor Refactor

--- apps/client/src/pages/user/workspace/[ws].tsx
+++ apps/client/src/pages/user/workspace/[ws].tsx
@@ -4,6 +4,7 @@ import { AxiosRequestConfig } from "axios";
 import {
   Box,
   Button,
+  CircularProgress,
   FormGroup,
   Input,
   Typography,
@@ -13,21 +14,30 @@ import { useRecoilValue } from "recoil";
 import { userAuthState, userLoadingState } from "@/store/selectors";
 import { config } from "dotenv";
 
+type UploadStatusType = { percentage: number, inProgress: boolean, completed: boolean, errorMsg: string }
+
+
 const Upload = () => {
   const [vidFile, setVidFile] = useState<File>();
   const [title, setTitle] = useState<string>();
   const [description, setDescription] = useState<string>();
   const [thumbnail, setThumnbail] = useState<File>();
+  const [uploadStatus, setUploadStatus] = useState<UploadStatusType>({
+    percentage: 0,
+    inProgress: false,
+    completed: false,
+    errorMsg: ""
+  })
 
   const isAuthenticated = useRecoilValue(userAuthState);
   const isLoading = useRecoilValue(userLoadingState);
+  const router = useRouter();
 
   useEffect(() => {
     if (!isAuthenticated && !isLoading) {
       router.push("/");
     }
   }, [isAuthenticated, isLoading]);
-  const router = useRouter();
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
@@ -41,7 +51,7 @@ const Upload = () => {
         let sliceEnd = Math.min(sliceStart + packetSize, vidFile.size);
         let fileSlice = vidFile.slice(sliceStart, sliceEnd);
 
-        formData.append("vidFile", fileSlice, `${partNum}`);
+        formData.set("vidFile", fileSlice, `${partNum}`);
         formData.set("total_parts", `${partsQty}`);
         formData.set("part_number", `${partNum}`);
         // formData.append("title", title);
@@ -53,33 +63,44 @@ const Upload = () => {
         headers.append('Content-Range', `bytes ${sliceStart}-${sliceEnd - 1}/${vidFile.size}`)
         headers.append("Part-To-Process", `${partNum}`);
         try {
-          // const xhr = new XMLHttpRequest();
-          // xhr.open("POST", `${process.env.NEXT_PUBLIC_serverURL}/workspace/${router.query.ws}/upload/video`, true);
-          // xhr.setRequestHeader('Content-Range', `bytes ${sliceStart}-${sliceEnd - 1}/${vidFile.size}`)
-          // xhr.withCredentials = true;
-          // xhr.send(formData)
           const res = await fetch(`${process.env.NEXT_PUBLIC_serverURL}/workspace/${router.query.ws}/upload/video`, {
             method: "POST",
             credentials: 'include',
             headers: headers,
             body: formData
           })
-          const { uploadId } = await res.json()
-          if (uploadId) {
-            formData.set("uploadId", uploadId)
-          }
-          console.log(uploadId);
           if (!res.ok) {
-            throw new Error("Fetch request error: " + res)
+            throw new Error("Fetch request error: " + res);
           }
-          // const res = await api2.post(
-          //   `/workspace/${router.query.ws}/upload/server`,
-          //   formData as AxiosRequestConfig<any>,
-          // );
-          // console.log(res.data);
-        } catch (error) {
+          var completedPercentage = Math.floor(partNum / partsQty * 100);
+          if (partNum == partsQty) {
+            setUploadStatus(prevState => ({
+              ...prevState,
+              percentage: completedPercentage,
+              inProgress: false,
+              completed: true,
+            }))
+          } else {
+            setUploadStatus(prevState => ({
+              ...prevState,
+              percentage: completedPercentage,
+              inProgress: true,
+            }))
+          }
+          const { uploadId } = await res.json();
+          if (uploadId) {
+            formData.set("uploadId", uploadId);
+          }
+        } catch (error: any) {
           console.log(error);
-          // break;
+          console.error("Error occurred while uploading part number " + partNum);
+          setUploadStatus(prevState => ({
+            ...prevState,
+            completed: false,
+            errorMsg: error.msg,
+            inProgress: false
+          }))
+          break;
         }
         sliceStart = sliceEnd;
       }
@@ -162,8 +183,20 @@ const Upload = () => {
           </Button>
         </form>
       </Box>
+      <UploadProgress uploadStatus={uploadStatus} />
     </div>
   );
 };
+
+const UploadProgress = ({ uploadStatus }: { uploadStatus: UploadStatusType }) => {
+  return (
+    <Box>
+      <CircularProgress variant="determinate" value={uploadStatus.percentage} />
+      {uploadStatus.inProgress && <Typography color={"red"}>In Progress</Typography>}
+      {uploadStatus.completed && <Typography variant="h3" color={"green"}>Upload Complete</Typography>}
+      {uploadStatus.errorMsg && <Typography color={"red"}>In Progress</Typography>}
+    </Box>
+  );
+}
 
 export default Upload;
